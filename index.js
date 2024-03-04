e1a165eadd358228120a4e8aedf36a6302a49416
MariannaRosso <98257895+MariannaRosso@users.noreply.github.com>
2024-03-04 13:03:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,68 +1,60 @@
 /* -----------------------------------------
-  Have focus outline only for keyboard users 
- ---------------------------------------- */
+   Have focus outline only for keyboard users 
+ ----------------------------------------- */
 
 const handleFirstTab = (e) => {
-  if(e.key === 'Tab') {
-    document.body.classList.add('user-is-tabbing')
-
-    window.removeEventListener('keydown', handleFirstTab)
-    window.addEventListener('mousedown', handleMouseDownOnce)
-  }
-
-}
+    if (e.key === 'Tab') {
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+        window.addEventListener('mousedown', handleMouseDownOnce);
+    }
+};
 
 const handleMouseDownOnce = () => {
-  document.body.classList.remove('user-is-tabbing')
+    document.body.classList.remove('user-is-tabbing');
+    window.removeEventListener('mousedown', handleMouseDownOnce);
+    window.addEventListener('keydown', handleFirstTab);
+};
 
-  window.removeEventListener('mousedown', handleMouseDownOnce)
-  window.addEventListener('keydown', handleFirstTab)
-}
-
-window.addEventListener('keydown', handleFirstTab)
+window.addEventListener('keydown', handleFirstTab);
 
 const backToTopButton = document.querySelector(".back-to-top");
 let isBackToTopRendered = false;
 
-let alterStyles = (isBackToTopRendered) => {
-  backToTopButton.style.visibility = isBackToTopRendered ? "visible" : "hidden";
-  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
-  backToTopButton.style.transform = isBackToTopRendered
-    ? "scale(1)"
-    : "scale(0)";
+const alterStyles = (rendered) => {
+    backToTopButton.style.visibility = rendered ? "visible" : "hidden";
+    backToTopButton.style.opacity = rendered ? 1 : 0;
+    backToTopButton.style.transform = rendered ? "scale(1)" : "scale(0)";
 };
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 700) {
-    isBackToTopRendered = true;
+    if (window.scrollY > 700) {
+        isBackToTopRendered = true;
+    } else {
+        isBackToTopRendered = false;
+    }
     alterStyles(isBackToTopRendered);
-  } else {
-    isBackToTopRendered = false;
-    alterStyles(isBackToTopRendered);
-  }
 });
 
-// Get the modal
+/* Image Modal Section */
 var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementsByClassName("visualization");
+var images = document.querySelectorAll(".visualization img"); // Ensure targeting <img> within .visualization
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 
 // Loop through all images if you have more than one
-for (let i = 0; i < img.length; i++) {
-    img[i].onclick = function(){
-      modal.style.display = "block";
-      modalImg.src = this.src;
-      captionText.innerHTML = this.alt;
-    }
-}
+images.forEach((image) => {
+    image.addEventListener('click', function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt; // Assuming alt text is meaningful
+    });
+});
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
+span.onclick = function () {
+    modal.style.display = "none";
+};
