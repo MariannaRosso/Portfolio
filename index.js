0dfe029fe6755dae8007c201f27845f86d46d1d3
MariannaRosso <98257895+MariannaRosso@users.noreply.github.com>
2024-03-04 12:55:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -41,3 +41,28 @@ window.addEventListener("scroll", () => {
     alterStyles(isBackToTopRendered);
   }
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("visualization");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+// Loop through all images if you have more than one
+for (let i = 0; i < img.length; i++) {
+    img[i].onclick = function(){
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+    }
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
